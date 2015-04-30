e10046f541403ffe3fce5d9eb34b6d19379407cb
sudeshgaur <gsudesh02@gmail.com>
2015-04-29 20:08:02 -0400

    Push

--- src/jake2/Defines.java
+++ src/jake2/Defines.java
@@ -39,7 +39,7 @@ public class Defines {
 	public final static int GRENADE_MINSPEED = 400;
 	public final static int GRENADE_MAXSPEED = 800;
 
-	// Trying to submit. Again.
+	//Again, I am Learning how to push.
 	// client/q_shared.h
 
 	// can accelerate and turn
